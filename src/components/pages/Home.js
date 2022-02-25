748eb8f5bce75a66206a3541868eb847a98d025c
Nikolas Oliveira <nikolas_081@hotmail.com>
2022-02-25 00:16:39 -0300

    Criação de algumas páginas e estilizações das mesmas

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton'
+
 function Home(){
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Costs</span></h1>
+            <p>Gerenciei projetos e outros!</p>
+            <LinkButton to='/newproject' text='Criar Projeto'/>
+            <img src={savings} alt='Costs'/>
+        </section>
     )
 }
 
