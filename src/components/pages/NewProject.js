9fc5381b197e81100afef6fad6b6d0e24e0acc89
Nikolas Oliveira <nikolas_081@hotmail.com>
2022-03-01 00:45:22 -0300

    Finalização da tela de adicionar projetos e da Projetos, os dados agora já dinâmicos com o db.json as a server, podendo adicionar e excluir direto do db

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,14 +1,41 @@
+import {useNavigate} from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 
 
 function NewProject(){
+
+    const navigate = useNavigate()
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost= 0
+        project.services= []
+
+        fetch('http://localhost:5000/projects',{
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json',
+            },
+            body: JSON.stringify(project),
+
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            navigate('/projects', {state: {message: 'Projeto criado com sucesso'}})
+        })
+        .catch(err=>console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
             <p>Crie o seu projeto para depois controlá-lo</p>
-            <ProjectForm btnText='Criar projeto'/>
+            <ProjectForm handleSubmit={createPost} btnText='Criar projeto'/>
         </div>
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,62 @@
+import { useLocation } from "react-router-dom"
+
+
+import Message from "../layouts/Message"
+import Container from '../layouts/Container'
+import LinkButton from "../layouts/LinkButton"
+import CardProject from "../project/CardProject"
+
+import styles from './Projects.module.css'
+import { useState, useEffect } from "react"
+
 function Projects(){
+
+    const[ projects, setProjects] = useState([])
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects',{
+        method: 'GET',
+        hearders: {
+            'Content-Type':'application/json',
+        },
+        
+        })
+        .then((resp) => resp.json())
+        .then(data => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
-        <h1>Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.tittle_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto'/>
+            </div>
+            {message && <Message type='sucess' msg={message} />}
+            <Container customClass='start'>
+            {projects.length > 0 && 
+            projects.map((project) =>(
+                <CardProject 
+                name={project.name} 
+                id={project.id}
+                budget={project.budget}
+                category={project.category.name}
+                key={project.id}
+
+                />
+            ))
+            }
+            </Container>
+        </div>
     )
 }
 
